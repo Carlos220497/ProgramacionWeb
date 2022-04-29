c95e3d67bd330019133f9c78eed6606464c58719
Carlos Ramos R <carlos.rr2204@gmail.com>
2022-04-29 16:47:11 -0500

    Creacion de una clase y objeto Student

---
+++ Semana01/src/main/java/pe/edu/upc/Program.java
@@ -0,0 +1,15 @@
+package pe.edu.upc;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Student student = new Student(); // creas un objeto
+
+		student.setFirstName("Carlos Ramos Rojas"); // invocación a los metodos y escribir el nombre
+
+		System.out.println(student.getFirstName()); // imprimir el valor del nombre en pantalla
+
+	}
+
+}
